32fe047bb5f2703a9b9d9e400952068090cd7d7b
Robiul Islam <therobiulislam12@gmail.com>
2024-08-31 04:59:53 +0600

    added first widget

--- robi-elekit.php
+++ robi-elekit.php
@@ -60,19 +60,27 @@ final class R_EleKit {
 
         add_action( 'plugins_loaded', [$this, 'rek_on_plugins_loaded'] );
 
+        add_action( 'elementor/widgets/register', [$this, 'register_hello_world_widget'] );
+
     }
 
+    /**
+     * Call all things here
+     *
+     * After all plugins loaded it'll be called
+     *
+     * @return void
+     */
+    public function rek_on_plugins_loaded() {
 
-	/**
-	 * Call all things here
-	 * 
-	 * After all plugins loaded it'll be called
-	 * 
-	 * @return void
-	 */
-	public function rek_on_plugins_loaded(){
-		
-	}
+    }
+
+    public function register_hello_world_widget( $widgets_manager ) {
+        require_once __DIR__ . '/widgets/hello-world-widget-2.php';
+
+        $widgets_manager->register( new \Elementor_Hello_World_Widget_2() );
+
+    }
 
     /**
      * Compatibility Checks
